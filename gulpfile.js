ddc0279eb2a6ed2f8e7786a496630731c414f13c
Roberto Hernandez <robertohdz.ti94@gmail.com>
2023-04-24 20:10:25 -0600

    lighten images

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,12 @@ const { src, dest, watch } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const plumber = require('gulp-plumber')
 
+//Importando dependencias para images
+const cache = require('gulp-cache')
+const imagemin = require('gulp-imagemin')
+const webp = require('gulp-webp')
+const avif = require('gulp-avif')
+
 //Funciones para trabajar con sass y gulp
 
 const css = done => {
@@ -11,6 +17,37 @@ const css = done => {
     .pipe( plumber() )
     .pipe( sass() )
     .pipe( dest('build/css'))
+    done()
+}
+
+//Funciones para optimizar las images
+const versionWebp = done => {
+    const options = {
+        quality: 50
+    }
+    src('src/img/**/*.{jpg,png}')
+    .pipe( webp( options ) )
+    .pipe( dest('build/img') )
+    done()
+}
+
+const versionAvif = done => {
+    const options = {
+        quality: 50
+    }
+    src('src/img/**/*.{jpg,png}')
+    .pipe( avif(options) )
+    .pipe( dest('build/img') )
+    done()
+}
+
+const versionJpg = done => {
+    const options = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{jpg,png}')
+    .pipe( cache( imagemin(options) ) )
+    .pipe( dest('build/img') )
     done()
 }
 
@@ -22,3 +59,6 @@ const dev = done => {
 
 exports.css = css
 exports.dev = dev
+exports.webp = versionWebp
+exports.avif = versionAvif
+exports.jpg = versionJpg
